88647ff951e20047e39e323f520fa6d6bd90ad71
zhushuangfei <zhushuangfei@intellifai.cn>
2018-01-23 17:45:34 +0800

    【添加swagger配置】

---
+++ src/main/java/cn/intellifai/Swagger2Config.java
@@ -0,0 +1,41 @@
+package cn.intellifai;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by zhushuangfei on 2017/11/1.
+ */
+@EnableSwagger2
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()//为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("cn.intellifai.Controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+    //构建 api文档的详细信息函数
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder() //页面标题
+                .title("Spring Boot 测试使用 Swagger2 构建RESTful API")
+                .termsOfServiceUrl("http://localhost/") //创建人
+                .contact("zhushuangfei") //版本号
+                .version("1.0") //描述
+                .description("API 描述")
+                .build();
+    }
+}
