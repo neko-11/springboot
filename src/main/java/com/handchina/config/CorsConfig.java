7d6a909a13743709ae8661fb75bddfe8103082f2
20983 <shuangfei.zhu@hand-china.com>
2018-07-23 15:55:33 +0800

    【更新】

--- src/main/java/com/handchina/config/CorsConfig.java
+++ src/main/java/com/handchina/config/CorsConfig.java
@@ -1,29 +1,21 @@
 package com.handchina.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- * 跨域拦截器
- * Created by zhushuangfei on 2017/12/28
+ * 跨域拦截器   需要允许跨域的Controller继承这个类
+ * 或者使用 @CrossOrigin   eg：@CrossOrigin(origins = "http://192.168.1.97:3000", maxAge = 3600)
+ * Created by zhushuangfei on 2018/7/21
  */
 @Configuration
-public class CorsConfig {
-    private CorsConfiguration buildConfig(){
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 1 设置访问源地址      corsConfiguration.addAllowedOrigin("http://edu.intellifai.cn");
-        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
-        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
-        return corsConfiguration;
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("//api/v1/education", buildConfig()); // 4 对接口配置跨域设置
-        return new CorsFilter(source);
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")//允许的路径
+                .allowedOrigins("http://127.0.0.1:3000","http://localhost:3000")//允许的ip和端口   .allowedOrigins("http://192.168.1.97:3000")
+                .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")//允许的方法
+                .allowCredentials(true).maxAge(3600);
     }
 }
